1e8acb2143ea83d33235e7cba9b0652d381d3d96
K.Takata <kentkt@csc.jp>
2016-08-27 08:00:49 +0900

    dist: Update pack.bat
    Include source codes in the package.

--- pack.bat
+++ pack.bat
@@ -1,21 +1,25 @@
 @echo off
 if "%1"=="" goto usage
-setlocal
+setlocal DISABLEDELAYEDEXPANSION
 
 :: Normal version
 set execfiles=tcdll.tclock tclock.exe tcplayer.exe tcprop.exe tcsntp.exe tctimer.exe
 set pkgfiles=readme-kt.txt config-kt.txt format-kt.txt readme.html
+set srcfiles=source source_all\config.h source_custom\config.h pack.bat make_all.bat prepare_custom.bat
 
-rem 7-zip32 a -mx=9 -m0=PPMd source.7z source pack.bat -xr!out -xr!out64 -xr!work -xr!work64 -xr!*.bak -xr!*.old
-rem 7-zip32 a -m0=PPMd:o=31:mem=25 source.7z source pack.bat -xr!out -xr!out64 -xr!work -xr!work64 -xr!*.bak -xr!*.old
+if not exist pkg mkdir pkg
+
+rem 7-zip32 a -mx=9 -m0=PPMd source.7z %srcfiles% -xr!out -xr!out64 -xr!work -xr!work64 -xr!*.bak -xr!*.old -xr!*.sw? -xr!*~ -xr!*.aps -xr!tags
+7-zip32 a -m0=PPMd:o=31:mem=25 source.7z %srcfiles% -xr!out -xr!out64 -xr!work -xr!work64 -xr!*.bak -xr!*.old -xr!*.sw? -xr!*~ -xr!*.aps -xr!tags
+move /y source.7z pkg > nul
 
 robocopy source\out   pkg\x86 %execfiles% > nul
 robocopy source\out64 pkg\x64 %execfiles% > nul
-robocopy lang pkg\lang > nul
+robocopy lang pkg\lang /xf *~ *.sw? > nul
 robocopy .    pkg %pkgfiles% > nul
 
 cd pkg
-7-zip32 a -mx=9 tclocklight-%1.zip %pkgfiles% lang x86 x64
+7-zip32 a -mx=9 tclocklight-%1.zip %pkgfiles% lang x86 x64 source.7z
 cd ..
 
 
